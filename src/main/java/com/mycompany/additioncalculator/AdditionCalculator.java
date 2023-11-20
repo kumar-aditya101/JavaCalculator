3b194f02c95277ba118b61634eb465caa4347f2f
Kumar Aditya <raazaditya513@gmail.com>
2023-11-20 23:48:55 +0530

    Calculator in Java

---
+++ src/main/java/com/mycompany/additioncalculator/AdditionCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.additioncalculator;
+
+/**
+ *
+ * @author KUMAR ADITYA
+ */
+public class AdditionCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
